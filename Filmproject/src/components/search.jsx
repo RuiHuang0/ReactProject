41658b6a27bc3d53ddc36a111bf164dbb7424de1
Rui Huang <rcetger1@gmail.com>
2025-06-22 18:45:21 +0200

    added cards search and debouncer

---
+++ Filmproject/src/components/search.jsx
@@ -0,0 +1,22 @@
+
+/*never mutate state or props only mutate state using setter*/
+
+function Search({searchTerm,setSearchTerm}) {
+
+    return (
+        <div className="search">
+            <div>
+                <img src="search.svg" alt="search" />
+
+                <input
+                    type="text"
+                    placeholder="Search through thousands of movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Search;
